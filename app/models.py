87e16d016bdfde5e66da3807264fb04c10f93efa
Daniel Vysotskyi <Vysotskyi-ext@fidor.com>
2020-02-18 18:38:32 +0200

    Add Get by id route for products, add query method and JSON repr method to Product model

--- app/models.py
+++ app/models.py
@@ -11,3 +11,24 @@ class Product(db.Model):
     product_price = db.Column(db.Float)
     product_name = db.Column(db.String(60))
     size = db.Column(db.String(10))
+
+    @staticmethod
+    def get_by_id(product_id):
+        """
+        Check a product by their product ids
+        :param product_id:
+        :return:
+        """
+        return Product.query.filter_by(product_id = product_id).first()
+
+    def json(self):
+        """
+        Json representation of the product model.
+        :return:
+        """
+        return {
+            'product_id': self.product_id,
+            'product_name': self.product_name,
+            'product_price': self.product_price,
+            'size': self.size
+        }
--- app/product/views.py
+++ app/product/views.py
@@ -1,11 +1,33 @@
 from flask import make_response, jsonify
-
+from app.models import Product
 from . import product
 
-@product.route('/products', methods=['GET'])
-def index():
+# TODO: Refactor requests to helpers
+@product.route('/products/<product_id>', methods=['GET'])
+def show(product_id):
     """
+    Return a product with the supplied product Id.
+    :param product_id: Product Id
+    :return:
     """
-    return make_response(jsonify({
-        'message': 'Working good!'
-    })), 200
+
+    try:
+        int(product_id)
+    except ValueError:
+        return make_response(jsonify({
+            'message': "Use valid product id"
+        })), 422
+    else:
+        product = Product.get_by_id(product_id)
+
+        if product:
+            return make_response(
+                jsonify(
+                    product.json()
+                )
+            ), 200
+        else:
+            return make_response(jsonify({
+                'message': "Product with id=%s doesn`t exist" % product_id
+            })), 404
+        
